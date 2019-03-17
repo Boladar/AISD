fb0c9c7eb6576390fd7f3994ff8c187997373fa1
KacpiCzeci <kacpi_szkola@wp.eu>
2019-03-17 18:25:26 +0100

    modified on local copy

---
+++ 1/insertionSort.py
@@ -0,0 +1,10 @@
+def insertionSort(tablica):
+    tymczasowa = 0
+    for i in range(len(tablica)):
+        tymczasowa = tablica[i]
+        j=i-1
+        while j>=0 and tablica[j]>tymczasowa:
+            tablica[j+1]=tablica[j]
+            j-=1
+        tablica[j+1]=tymczasowa
+    return tablica
---
+++ 1/shellSort.py
@@ -0,0 +1,17 @@
+def przerwa(tablica):
+    h=0
+    while h<len(tablica):
+        h=3*h+1
+    return h
+def shellSort(tablica):
+    h=przerwa(tablica)
+    while (h):
+        for i in range(h,len(tablica),h):
+            tymczasowa=tablica[i]
+            j=i-h
+            while j>=0 and tablica[j]>tymczasowa:
+                tablica[j+h]=tablica[j]
+                j-=h
+            tablica[j+h]=tymczasowa
+        h=(h-1)/3
+    return tablica
