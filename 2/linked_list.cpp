50affc8d6859cb5d89ea5e2f6a013ed65edee8fc
Unknown <aleksander.szamalek@gmail.com>
2019-04-09 18:05:13 +0200

    init

---
+++ 2/linked_list.cpp
@@ -0,0 +1,23 @@
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+
+class linked_list
+{
+private:
+    /* data */
+public:
+    linked_list(/* args */);
+    ~linked_list();
+};
+
+linked_list::linked_list(/* args */)
+{
+}
+
+linked_list::~linked_list()
+{
+}
---
+++ 2/main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include "linked_list.cpp"
+
+using namespace std;
+
+int main(){
+    
+    
+    return 0;
+}
